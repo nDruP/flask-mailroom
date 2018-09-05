48001dad467362f955bd9a3e42c074c2d0f45f48
Andrew Park <apark46.work@gmail.com>
2018-09-05 00:37:11 -0700

    Lesson01|mailroom Successfully implemented projections, had to refactor donor_info(). Nav bar is implemented, everything seems good to go

--- main.py
+++ main.py
@@ -20,8 +20,7 @@ def report():
 
 
 @app.route('/add_donation', methods=['GET', 'POST'])
-def add_donation():
-    
+def add_donation():    
     if request.method == 'POST':
         donor_name = request.form['donor']
         amount = request.form['gift']
@@ -38,17 +37,39 @@ def add_donation():
 
 @app.route('/thank_you')
 def thank_you():
+    if request.method == 'POST':
+        donor_name = request.form['donor']
+        amount = request.form['gift']
+        try:
+            donor = Donor.select().where(Donor.name == donor_name).get()
+        except Donor.DoesNotExist:
+            donor = Donor(name=donor_name)
+            donor.save()
+        Donation(donor=donor, value=amount).save()
+        return redirect(url_for('thank_you', name=donor_name, amount=amount))
     return render_template('thank_you.jinja2',
                            name=request.args.get('name'),
                            amount=request.args.get('amount'))
 
 
-def donor_info():
+@app.route('/projection', methods=['GET', 'POST'])
+def run_projection():
+    if request.method == 'POST':
+        maximum = request.form['max']
+        minimum = request.form['min']
+        factor = request.form['factor']
+        return render_template('projection.jinja2',
+                               info = donor_info(minimum, maximum, factor))
+    return render_template('projection.jinja2')
+
+
+def donor_info(minimum=0, maximum=99999999999999999, factor=1):
     info = (Donation
             .select(Donation.donor,
                     fn.COUNT(Donation.value).alias('num_gift'),
-                    fn.SUM(Donation.value).alias('sum_gift'),
-                    fn.AVG(Donation.value).alias('avg_gift'))
+                    fn.SUM(factor*Donation.value).alias('sum_gift'),
+                    fn.AVG(factor*Donation.value).alias('avg_gift'))
+            .where(Donation.value >= minimum, Donation.value <= maximum)
             .group_by(Donation.donor)
             .order_by(Donation.donor))
     return info
